14ee30536ea07e0ecb3003cc11d3834863eb8201
ashutoshkashyap04 <kashyapashutoshkashyap@gmail.com>
2026-04-05 21:03:38 +0530

    Added Expense Management System Project

---
+++ main.py
@@ -0,0 +1,93 @@
+#Expense Management System
+
+class Expense:
+
+    def __init__(self, date, amount,  category, payment_method, description):
+        self.date = date
+        self.amount = amount
+        self.category = category
+        self.payment_method = payment_method
+        self.description = description
+
+    def display_expense(self):
+        print(f"Date: {self.date}, Amount: {self.amount}, Category: {self.category}, Payment Method: {self.payment_method}, Description: {self.description}")
+
+
+
+if __name__ == "__main__":
+    def Menu_display():
+        print("Expense Management System")
+        print("1. Add Expense")
+        print("2. View Expenses")
+        print("3. Reset Expenses")
+        print("4. Update Expenses")
+        print("5. Exit")        
+
+    while True:
+        Menu_display()
+        choice = int(input("Enter your choice: "))
+
+
+        if choice == 1:
+            date = input("Enter date (DD-MM-YYYY): ")
+            amount = float(input("Enter amount: "))
+            category = input("Enter category(e.g., Food, Transport, Utilities , Shopping): ")
+            payment_method = input("Enter payment method (e.g., Cash, Credit Card, UPI): ")
+            description = input("Enter description: ")
+            expense = Expense(date, amount, category, payment_method, description)
+            with open("Project 4/expenses.csv", "a") as file:
+                file.write(f"{date},{amount},{category},{payment_method},{description}\n")
+                print("Expense added successfully!")
+
+
+        elif choice == 2:
+            print("Expenses:")
+            try:
+                with open('Project 4/expenses.csv', 'r') as file:
+                    for line in file:
+                        date, amount, category, payment_method, description = line.strip().split(',')
+                        expense = Expense(date, float(amount), category, payment_method, description)
+                        expense.display_expense()
+            except FileNotFoundError:
+                print("No expenses recorded yet.")
+
+
+        elif choice == 3:
+            confirmation = input("Are you sure you want to reset all expenses? (YES/NO): ")
+            if confirmation.upper() == "YES":
+                open("Project 4/expenses.csv", "w").close()
+                print("All expenses have been reset.")
+            else:
+                print("Reset operation cancelled.")
+
+
+        elif choice == 4:
+            print("Showing current expenses: ")
+            
+            for i, line in enumerate(open('Project 4/expenses.csv', 'r').readlines()):
+                print(f"{i+1} -> {line.strip()}")
+
+
+            line_number_to_update=int(input("Enter the line number of the expense to update (starting from 1): "))
+            lines = open('Project 4/expenses.csv', 'r').readlines()
+
+            columns=lines[line_number_to_update - 1].strip().split(',')
+            print("1.Date  , 2.Amount  , 3.Category  , 4.Payment Method  , 5.Description")
+            column_number_to_update=int(input("Enter the column number to update(1-5): "))
+
+            new_value=input("Enter the new value: ")
+            columns[column_number_to_update - 1] = new_value
+
+            lines[line_number_to_update - 1] = ','.join(columns) + '\n'
+
+            with open('expenses.csv', 'w') as file:
+                file.writelines(lines)
+
+            print("Expense updated successfully!")
+
+            
+
+
+        elif choice == 5:
+            print("Exiting the program.")
+            break
