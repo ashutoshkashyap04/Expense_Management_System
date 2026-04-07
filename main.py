27c50c1105a6d87340e42e433db2d9e8aab57fb7
ashutoshkashyap04 <kashyapashutoshkashyap@gmail.com>
2026-04-07 13:05:48 +0530

    https://github.com/ashutoshkashyap04/Expense_Management_System

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
             payment_method = input("Enter payment method (e.g., Cash, Credit Card, UPI): ")
             description = input("Enter description: ")
             expense = Expense(date, amount, category, payment_method, description)
-            with open("Project 4/expenses.csv", "a") as file:
+            with open("expenses.csv", "a") as file:
                 file.write(f"{date},{amount},{category},{payment_method},{description}\n")
                 print("Expense added successfully!")
 
@@ -43,7 +43,7 @@ if __name__ == "__main__":
         elif choice == 2:
             print("Expenses:")
             try:
-                with open('Project 4/expenses.csv', 'r') as file:
+                with open('expenses.csv', 'r') as file:
                     for line in file:
                         date, amount, category, payment_method, description = line.strip().split(',')
                         expense = Expense(date, float(amount), category, payment_method, description)
@@ -55,7 +55,7 @@ if __name__ == "__main__":
         elif choice == 3:
             confirmation = input("Are you sure you want to reset all expenses? (YES/NO): ")
             if confirmation.upper() == "YES":
-                open("Project 4/expenses.csv", "w").close()
+                open("expenses.csv", "w").close()
                 print("All expenses have been reset.")
             else:
                 print("Reset operation cancelled.")
@@ -64,12 +64,12 @@ if __name__ == "__main__":
         elif choice == 4:
             print("Showing current expenses: ")
             
-            for i, line in enumerate(open('Project 4/expenses.csv', 'r').readlines()):
+            for i, line in enumerate(open('expenses.csv', 'r').readlines()):
                 print(f"{i+1} -> {line.strip()}")
 
 
             line_number_to_update=int(input("Enter the line number of the expense to update (starting from 1): "))
-            lines = open('Project 4/expenses.csv', 'r').readlines()
+            lines = open('expenses.csv', 'r').readlines()
 
             columns=lines[line_number_to_update - 1].strip().split(',')
             print("1.Date  , 2.Amount  , 3.Category  , 4.Payment Method  , 5.Description")
